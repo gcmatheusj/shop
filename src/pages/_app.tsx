4ba9e68ca6823aaeb54ded9554d5b949aa49da45
Matheus Castro <goca1377@gmail.com>
2022-11-06 19:40:41 -0300

    feat: add home styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
 import { globalStyles } from '../styles/global'
 
 import logoImg from '../assets/logo.svg'
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} />
+        <Image src={logoImg} alt='Ignite Shop Logo' />
       </Header>
       <Component {...pageProps} />
     </Container>
