f755e9ad86bd798ad3e94d332554bfc0c743cdf9
Matheus Castro <goca1377@gmail.com>
2022-12-19 08:35:28 -0300

    feat: add product ui

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,22 @@
-export default function ProductDetail () {
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+
+export default function Product () {
   return (
-    <h1>Product Detail</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, dignissimos! 
+          Quo molestiae vero excepturi odio fuga et, enim iure! Dolorem assumenda qui veniam iusto. 
+          Nisi cum dicta ex id eius?
+        </p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
