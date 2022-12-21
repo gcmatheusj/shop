26fecd5d9658189a21a8eef442c35c2bbef51caf
Matheus Castro <goca1377@gmail.com>
2022-12-21 09:56:01 -0300

    feat: improve SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
-import axios from "axios";
-import { GetStaticPaths, GetStaticProps } from "next";
-import Image from "next/image";
 import { useState } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import axios from "axios";
 import Stripe from "stripe";
+
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
@@ -39,24 +41,29 @@ export default function Product ({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt='' />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt='' />
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
-        <p>{product.description}</p>
-        <button 
-          type="button" 
-          disabled={isCreatingCheckoutSession} 
-          onClick={handleBuyProduct}
-        >
-          Comprar Agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+          <button 
+            type="button" 
+            disabled={isCreatingCheckoutSession} 
+            onClick={handleBuyProduct}
+          >
+            Comprar Agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
