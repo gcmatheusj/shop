57b91e62f0477326688d3fa6509377cf1dc7e2f6
Matheus Castro <goca1377@gmail.com>
2022-12-21 09:36:14 -0300

    feat: get real data on success page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
 import { useState } from "react";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
@@ -40,7 +41,7 @@ export default function Product ({ product }: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={480} alt='' />
       </ImageContainer>
 
       <ProductDetails>
